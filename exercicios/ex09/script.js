068fb6b6ef35f85a90a788e63f11c1c821117e55
pphols <pphols.reigota@gmail.com>
2022-08-12 10:51:00 -0300

    exercício 'hora do dia!'

--- exercicios/ex09/script.js
+++ exercicios/ex09/script.js
@@ -3,5 +3,19 @@ function carregar() {
   var img = document.getElementById('imagem');
   var data = new Date();
   var hora = data.getHours();
-  msg.innerHTML = `Agora são ${hora} horas`
+  msg.innerHTML = `Agora são ${hora} horas`;
+
+  if (hora >= 0 && hora < 12) {
+    // bom dia
+    img.src = 'img/manha.png';
+    document.body.style.background = '#A9B9D9';
+  } else if (hora >= 12 && hora <= 18){
+    // boa tarde
+    img.src = 'img/tarde.png';
+    document.body.style.background = '#D9A577';
+  } else{
+    // boa noite
+    img.src = 'img/noite.png';
+    document.body.style.background = '#025E73';
+  }
 }
